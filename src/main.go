55eff9f97b7f7fc7905b1dfb73442fa2bad95261
Jose <WateryOwl8942@gmail.com>
2020-10-04 15:17:02 -0600

    started dev

--- src/main.go
+++ src/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + "authentication token")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(discord)
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
